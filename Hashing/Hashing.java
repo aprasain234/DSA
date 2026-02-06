950fb60e4a8e8c54dd9fc84e311b23ae4b3e4334
Ashish Prasain <40052138+aprasain234@users.noreply.github.com>
2026-02-05 22:17:42 -0600

    LC 1189

--- Hashing/Hashing.java
+++
@@ -1,6 +0,0 @@
-public class Hashing {
-    public static void main(String[] args) {
-        HashMap<Integer, String> hashMap = new HashMap<Integer, String>();
-        hashMap.put(10, "Ashish");
-    }
-}
