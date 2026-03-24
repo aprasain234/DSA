58eadc7f83c44fc157764c708cf7aff0155ecf55
Ashish Prasain <aprasain345@gmail.com>
2026-03-23 20:45:25 -0500

    hashtable and PrefixSum.txt

--- src/Main.java
+++ src/Main.java
@@ -4,3 +4,6 @@ public class Main {
         System.out.println("Hello World");
     }
 }
+
+// Java focus: Get fluent with HashMap, HashSet, Arrays.sort(), String.charAt(), StringBuilder
+// Java focus: Deque<>, LinkedList, Stack (use ArrayDeque instead), recursive tree patterns
