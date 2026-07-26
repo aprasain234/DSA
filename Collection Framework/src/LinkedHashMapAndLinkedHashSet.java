09dcc0de15917b2631b68605f1a1949293b81221
Ashish Prasain <aprasain345@gmail.com>
2026-07-26 11:42:13 -0500

    LinkedHashMapAndLinkedHashSet basics

---
+++ Collection Framework/src/LinkedHashMapAndLinkedHashSet.java
@@ -0,0 +1,8 @@
+public class LinkedHashMapAndLinkedHashSet {
+    public static void main(String[] args) {
+        /*
+        LinkedHashMap maintains insertion order, which is important for specific problems like LRU Cache:
+        LinkedHashSet similarly maintains insertion order for sets. Use these when the order in which elements were inserted matters.
+         */
+    }
+}
